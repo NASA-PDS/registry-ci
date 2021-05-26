f26cd2723919224422bdc1ee5bf6f87ee6b71f91
Eugene <test@test.com>
2021-05-25 17:50:47 -0700

    Elasticsearch health check

--- docker/scripts/run_es.sh
+++ docker/scripts/run_es.sh
@@ -7,4 +7,8 @@ docker run \
   --detach \
   -p 9200:9200 \
   -e "discovery.type=single-node" \
+  --health-cmd "curl localhost:9200" \
+  --health-interval 10s \
+  --health-timeout 5s \
+  --health-retries 5 \
   docker.elastic.co/elasticsearch/elasticsearch-oss:7.10.2
